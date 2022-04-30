bea729dacd77ce161b44cef950cb3b9868cc33c0
Soraia Novaes <sonovaes511@gmail.com>
2022-04-30 00:29:18 -0300

    return create_philos

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: snovaes <snovaes@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 18:09:01 by snovaes           #+#    #+#             */
-/*   Updated: 2022/04/29 23:37:25 by snovaes          ###   ########.fr       */
+/*   Updated: 2022/04/30 00:28:26 by snovaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,11 @@ static void	create_philos(t_info *info)
 	int			i;
 	pthread_t	th_id;
 
-	info->firststamp = timestamp();
-//	gettimeofday(&info->create_at, NULL);
+	gettimeofday(&info->create_at, NULL);
 	i = 0;
 	while (i < info->num_of_philo)
 	{
-		info->philos[i].lasttimetoeat = info->firststamp;
+		info->philos[i].last_time_to_eat = info->create_at;
 		pthread_create(&info->philos[i].th_id, NULL, philo, &info->philos[i]);
 		pthread_create(&th_id, NULL, monitor, &info->philos[i]);
 		pthread_detach(th_id);
