3c97e743bf325bf4dd9afa7712238963c59119d7
Soraia Novaes <sonovaes511@gmail.com>
2022-04-30 13:44:56 -0300

    msleep in philo

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: snovaes <snovaes@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 19:36:14 by snovaes           #+#    #+#             */
-/*   Updated: 2022/04/30 04:06:43 by snovaes          ###   ########.fr       */
+/*   Updated: 2022/04/30 13:44:29 by snovaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*philo(void *argv)
 	philo = argv;
 	if (philo->n % 2)
 		//usleep(philo->info->time_to_eat * 1000);
-		msleep(5);
+		msleep(philo->info->time_to_eat);
 	while (!has_finished(philo->info))
 	{
 		if (pickup_fork(philo))
